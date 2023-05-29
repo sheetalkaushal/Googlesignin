50041ffb2b91df96c0a716048845e7e4cedb89ed
sheetalkaushal <sheetalkaushal2512@gmail.com>
2023-05-29 10:03:59 +0530

    first

--- src/Screens/Cube/Cube.js
+++ src/Screens/Cube/Cube.js
@@ -1,67 +1,3 @@
-// import {View, Text, StyleSheet, Image} from 'react-native';
-// import React from 'react';
-// import imagePath from '../../constants/imagePath';
-// const Cube = () => {
-//   return (
-//     <View style={styles.container}>
-//       <View style={[styles.face, styles.frontFace]}>
-//         <Image style={{width: 100, height: 160}} source={imagePath.icWeather} />
-//       </View>
-//       <View style={[styles.face, styles.backFace]}>
-//         <Image style={{width: 100, height: 160}} source={imagePath.icback} />
-//       </View>
-//       <View style={[styles.face, styles.leftFace]}>
-//         <Image style={{width: 100, height: 160}} source={imagePath.icbanner1} />
-//       </View>
-//       <View style={[styles.face, styles.rightFace]} />
-//       <View style={[styles.face, styles.topFace]} />
-//       <View style={[styles.face, styles.bottomFace]} />
-//     </View>
-//   );
-// };
-
-// const styles = StyleSheet.create({
-//   container: {
-//     width: 200,
-//     height: 200,
-//     transformStyle: 'preserve-3d',
-//     perspective: 1000,
-//   },
-//   face: {
-//     position: 'absolute',
-//     width: 200,
-//     height: 200,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//     opacity: 0.8,
-//   },
-//   frontFace: {
-//     // backgroundColor: 'yellow',
-//     transform: [{rotateY: '30deg'}, {translateX: 100}, {translateY: 100}],
-//   },
-//   backFace: {
-//     // backgroundColor: 'green',
-//     transform: [{rotateY: '-30deg'}, {translateX: 0}, {translateY: 100}],
-//   },
-//   leftFace: {
-//     // backgroundColor: 'yellow',
-//     transform: [{rotateY: '-90deg'}, {translateX: -100}, {translateY: 0}],
-//   },
-//   rightFace: {
-//     // backgroundColor: 'yellow',
-//     transform: [{rotateY: '90deg'}, {translateX: 100}, {translateY: 0}],
-//   },
-//   topFace: {
-//     // backgroundColor: 'orange',
-//     transform: [{rotateX: '-60deg'}, {translateY: -100}, {translateX: 0}],
-//   },
-//   bottomFace: {
-//     // backgroundColor: 'purple',
-//     transform: [{rotateX: '90deg'}, {translateY: 100}, {translateX: 0}],
-//   },
-// });
-
-// export default Cube;
 import React, {Component} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import {
@@ -81,7 +17,7 @@ const Cube = () => {
   const images = useImage(
     'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg',
   );
-  
+
   const leftstar = Skia.Path.MakeFromSVGString(
     'M 0 0 L 100 30 L 100 151 L 0 120 Z',
   );
@@ -89,11 +25,10 @@ const Cube = () => {
     'M 0 30 L 100 0 L 100 120 L 0 151 Z',
   );
   const Topstar = Skia.Path.MakeFromSVGString(
-    'M 99 35 L 0 70 L 100 100 L 201 70 Z',     
+    'M 99 35 L 0 70 L 100 100 L 201 70 Z',
   );
   return (
     <>
-    
       <Canvas
         style={{
           flex: 1,
